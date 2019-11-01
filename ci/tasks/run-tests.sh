7c64ea0398c7f29dca7c72a32722d02ec35f3114
Justin Carter <justin@misthos.io>
2019-11-01 06:33:21 +0100

    Typo

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -3,7 +3,7 @@
 set -eu
 
 export CARGO_HOME="$(pwd)/cargo-home"
-export CARGO_TARGET_DIR="$(pwd)/cargo-targot-dir"
+export CARGO_TARGET_DIR="$(pwd)/cargo-target-dir"
 export GRADLE_USER_HOME="$(pwd)/gradle-home"
 
 pushd repo
